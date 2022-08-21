ab03295b79b3474fa49e1508b52fab918ac72bcb
Zabala22 <104261038+Zabala22@users.noreply.github.com>
2022-08-21 13:58:07 +0800

    Add files via upload

---
+++ Research/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Research
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.nobutton = new System.Windows.Forms.Button();
+            this.yesbutton = new System.Windows.Forms.Button();
+            this.babalikkaba = new System.Windows.Forms.Label();
+            this.hicrush = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.image = new System.Windows.Forms.PictureBox();
+            this.image7 = new System.Windows.Forms.PictureBox();
+            this.image4 = new System.Windows.Forms.PictureBox();
+            this.image3 = new System.Windows.Forms.PictureBox();
+            this.image6 = new System.Windows.Forms.PictureBox();
+            this.image8 = new System.Windows.Forms.PictureBox();
+            this.image2 = new System.Windows.Forms.PictureBox();
+            this.image5 = new System.Windows.Forms.PictureBox();
+            this.image1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MistyRose;
+            this.panel1.Controls.Add(this.image);
+            this.panel1.Controls.Add(this.image7);
+            this.panel1.Controls.Add(this.image4);
+            this.panel1.Controls.Add(this.image3);
+            this.panel1.Controls.Add(this.image6);
+            this.panel1.Controls.Add(this.image8);
+            this.panel1.Controls.Add(this.image2);
+            this.panel1.Controls.Add(this.image5);
+            this.panel1.Controls.Add(this.image1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.nobutton);
+            this.panel1.Controls.Add(this.yesbutton);
+            this.panel1.Controls.Add(this.babalikkaba);
+            this.panel1.Controls.Add(this.hicrush);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(831, 568);
+            this.panel1.TabIndex = 4;
+            // 
+            // nobutton
+            // 
+            this.nobutton.BackColor = System.Drawing.Color.White;
+            this.nobutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.nobutton.Location = new System.Drawing.Point(437, 276);
+            this.nobutton.Name = "nobutton";
+            this.nobutton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.nobutton.Size = new System.Drawing.Size(81, 37);
+            this.nobutton.TabIndex = 10;
+            this.nobutton.Text = "NO";
+            this.nobutton.UseVisualStyleBackColor = false;
+            this.nobutton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // yesbutton
+            // 
+            this.yesbutton.BackColor = System.Drawing.Color.White;
+            this.yesbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.yesbutton.Location = new System.Drawing.Point(317, 276);
+            this.yesbutton.Name = "yesbutton";
+            this.yesbutton.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.yesbutton.Size = new System.Drawing.Size(81, 37);
+            this.yesbutton.TabIndex = 9;
+            this.yesbutton.Text = "YES";
+            this.yesbutton.UseVisualStyleBackColor = false;
+            this.yesbutton.Click += new System.EventHandler(this.yesbutton_Click);
+            // 
+            // babalikkaba
+            // 
+            this.babalikkaba.AutoSize = true;
+            this.babalikkaba.Font = new System.Drawing.Font("Candara Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.babalikkaba.Location = new System.Drawing.Point(313, 227);
+            this.babalikkaba.Name = "babalikkaba";
+            this.babalikkaba.Size = new System.Drawing.Size(205, 23);
+            this.babalikkaba.TabIndex = 8;
+            this.babalikkaba.Text = "I love you, babalik ka ba?";
+            // 
+            // hicrush
+            // 
+            this.hicrush.AutoSize = true;
+            this.hicrush.Font = new System.Drawing.Font("Cooper Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hicrush.ForeColor = System.Drawing.Color.Firebrick;
+            this.hicrush.Location = new System.Drawing.Point(336, 200);
+            this.hicrush.Name = "hicrush";
+            this.hicrush.Size = new System.Drawing.Size(143, 27);
+            this.hicrush.TabIndex = 7;
+            this.hicrush.Text = "HI CRUSH!";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 550);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Adie pogi hehehe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 550);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Made by:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // image
+            // 
+            this.image.Image = global::Research.Properties.Resources.pointer;
+            this.image.Location = new System.Drawing.Point(214, 262);
+            this.image.Name = "image";
+            this.image.Size = new System.Drawing.Size(100, 67);
+            this.image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image.TabIndex = 23;
+            this.image.TabStop = false;
+            // 
+            // image7
+            // 
+            this.image7.Image = global::Research.Properties.Resources.no7;
+            this.image7.Location = new System.Drawing.Point(317, 433);
+            this.image7.Name = "image7";
+            this.image7.Size = new System.Drawing.Size(198, 121);
+            this.image7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image7.TabIndex = 22;
+            this.image7.TabStop = false;
+            // 
+            // image4
+            // 
+            this.image4.Image = global::Research.Properties.Resources.no6;
+            this.image4.Location = new System.Drawing.Point(424, 22);
+            this.image4.Name = "image4";
+            this.image4.Size = new System.Drawing.Size(176, 120);
+            this.image4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image4.TabIndex = 21;
+            this.image4.TabStop = false;
+            // 
+            // image3
+            // 
+            this.image3.Image = global::Research.Properties.Resources.no4;
+            this.image3.Location = new System.Drawing.Point(214, 40);
+            this.image3.Name = "image3";
+            this.image3.Size = new System.Drawing.Size(174, 113);
+            this.image3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image3.TabIndex = 20;
+            this.image3.TabStop = false;
+            // 
+            // image6
+            // 
+            this.image6.Image = global::Research.Properties.Resources.no5;
+            this.image6.Location = new System.Drawing.Point(105, 419);
+            this.image6.Name = "image6";
+            this.image6.Size = new System.Drawing.Size(117, 128);
+            this.image6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image6.TabIndex = 19;
+            this.image6.TabStop = false;
+            // 
+            // image8
+            // 
+            this.image8.Image = global::Research.Properties.Resources.no3;
+            this.image8.Location = new System.Drawing.Point(35, 262);
+            this.image8.Name = "image8";
+            this.image8.Size = new System.Drawing.Size(145, 103);
+            this.image8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image8.TabIndex = 15;
+            this.image8.TabStop = false;
+            // 
+            // image2
+            // 
+            this.image2.Image = global::Research.Properties.Resources.no2;
+            this.image2.Location = new System.Drawing.Point(512, 328);
+            this.image2.Name = "image2";
+            this.image2.Size = new System.Drawing.Size(152, 90);
+            this.image2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image2.TabIndex = 14;
+            this.image2.TabStop = false;
+            // 
+            // image5
+            // 
+            this.image5.Image = global::Research.Properties.Resources.no1;
+            this.image5.Location = new System.Drawing.Point(683, 188);
+            this.image5.Name = "image5";
+            this.image5.Size = new System.Drawing.Size(136, 102);
+            this.image5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image5.TabIndex = 13;
+            this.image5.TabStop = false;
+            // 
+            // image1
+            // 
+            this.image1.Image = global::Research.Properties.Resources.no0;
+            this.image1.Location = new System.Drawing.Point(12, 88);
+            this.image1.Name = "image1";
+            this.image1.Size = new System.Drawing.Size(137, 146);
+            this.image1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image1.TabIndex = 12;
+            this.image1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Research.Properties.Resources._67Pg;
+            this.pictureBox1.Location = new System.Drawing.Point(472, 199);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 25);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(831, 566);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "I miss u";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.image)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button nobutton;
+        private System.Windows.Forms.Button yesbutton;
+        private System.Windows.Forms.Label babalikkaba;
+        private System.Windows.Forms.Label hicrush;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox image4;
+        private System.Windows.Forms.PictureBox image3;
+        private System.Windows.Forms.PictureBox image6;
+        private System.Windows.Forms.PictureBox image8;
+        private System.Windows.Forms.PictureBox image2;
+        private System.Windows.Forms.PictureBox image5;
+        private System.Windows.Forms.PictureBox image1;
+        private System.Windows.Forms.PictureBox image7;
+        private System.Windows.Forms.PictureBox image;
+    }
+}
+
---
+++ Research/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Research
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.InitializeComponent();
+            image.Visible = false;
+            image1.Visible = false;
+            image2.Visible = false;
+            image3.Visible = false;
+            image4.Visible = false;
+            image5.Visible = false;
+            image6.Visible = false;
+            image7.Visible = false;
+            image8.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = nobutton.Location.X;
+            int y = nobutton.Location.Y;
+            nobutton.Location = new Point(528, 175);
+            image.Visible = true;
+
+            if (x == 528)
+            {
+                nobutton.Location = new Point(12, 12);
+                yesbutton.Width = 91;
+                yesbutton.Height = 47;
+            }
+            if (x == 12)
+            {
+                nobutton.Location = new Point(738, 517);
+                
+            }
+            if (x == 738)
+            {
+                nobutton.Location = new Point(109, 377);
+                yesbutton.Width = 101;
+                yesbutton.Height = 57;
+            }
+            if (x == 109)
+            {
+                nobutton.Location = new Point(528, 175);
+                image1.Visible = true;
+
+            }
+            if (x == 528)
+            {
+                nobutton.Location = new Point(12, 12);
+                yesbutton.Width = 106;
+                yesbutton.Height = 62;
+                image2.Visible = true;
+            }
+            if (x == 12)
+            {
+                nobutton.Location = new Point(738, 517);
+                image3.Visible = true;
+
+            }
+            if (x == 738)
+            {
+                nobutton.Location = new Point(109, 377);
+                yesbutton.Width = 111;
+                yesbutton.Height = 67;
+                image4.Visible = true;
+            }
+            if (x == 109)
+            {
+                nobutton.Location = new Point(394, 399);
+                image5.Visible = true;
+
+            }
+            if (x == 394)
+            {
+                nobutton.Location = new Point(606, 118);
+                image6.Visible = true;
+
+            }
+            if (x == 606)
+            {
+                nobutton.Location = new Point(179, 187);
+                image7.Visible = true;
+
+
+            }
+            if (x == 179)
+            {
+                nobutton.Location = new Point(437, 276);
+                nobutton.Enabled = false;
+                image8.Visible = true;
+
+            }
+        }
+
+        private void yesbutton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 form2 = new Form2();
+            form2.Show();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
